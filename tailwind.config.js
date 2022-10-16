c5e4e7740a80a097a3fcb9d78db087857438573e
Fuego <hoanganhjalai12@gmail.com>
2022-10-16 17:42:41 +0700

    v2

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage:{
+        'bg-red':'linear-gradient(180deg, rgba(231, 29, 54, 0.6) 0%, rgba(231, 29, 54, 0) 100%),linear-gradient(0deg, #FFFFFF, #FFFFFF)'
+      }
+    },
   },
   plugins: [],
 }
