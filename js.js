4ebc2c61e4696e3afe1948651abc71ee9511d640
hoanganhfuego <hoanganhjalai12@gmail.com>
2022-10-17 20:42:32 +0700

    v4

---
+++ js.js
@@ -0,0 +1,26 @@
+const aside = document.querySelectorAll('.aside')[0]
+const bar = document.querySelectorAll('.bar')[0]
+const asideBg = document.querySelectorAll('.aside-bg')[0]
+const header = document.querySelectorAll('.header-navbar')[0]
+
+function handleScoll(){
+    if (window.pageYOffset >= header.offsetTop + 50) {
+        header.classList.add("sticky-navbar")
+    } else {
+        header.classList.remove("sticky-navbar");
+    }
+}
+
+function handleAsideSliceIn(){
+    aside.setAttribute('style', 'left: 0px')
+    asideBg.setAttribute('style', 'display: block')
+}
+
+function handleAsideSliceOut(){
+    aside.removeAttribute('style')
+    asideBg.removeAttribute('style')
+}
+
+window.onscroll = handleScoll
+bar.addEventListener('click', handleAsideSliceIn)
+asideBg.addEventListener('click', handleAsideSliceOut)
